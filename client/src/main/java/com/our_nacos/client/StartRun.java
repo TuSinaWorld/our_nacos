56b61b7db57b7144508da2d58f476a54bb4cc886
zjyjl123 <96461652+zjyjl123@users.noreply.github.com>
2023-05-11 21:06:41 +0800

    服务注册包装

--- client/src/main/java/com/our_nacos/client/StartRun.java
+++ client/src/main/java/com/our_nacos/client/StartRun.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.our_nacos.client"})
-@OurEnableDiscoveryClient(autoregister = false)
+@OurEnableDiscoveryClient
 public class StartRun {
     public static void main(String[] args) {
         SpringApplication.run(StartRun.class);

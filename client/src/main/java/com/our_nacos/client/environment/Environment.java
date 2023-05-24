28d615ecfebb097e7714555cd03db4643748e0ad
tanyaoli <1607667368@qq.com>
2023-05-24 20:16:09 +0800

    完善获取内存的方法

--- client/src/main/java/com/our_nacos/client/environment/Environment.java
+++ client/src/main/java/com/our_nacos/client/environment/Environment.java
@@ -1,6 +1,43 @@
 package com.our_nacos.client.environment;
 
+import org.springframework.beans.factory.annotation.Value;
+
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
 public class Environment {
-    private  double free_space;
-    private double total_space;
+    @Value("${ourstorage.localspace}")
+    private String space;
+
+    private BigDecimal free_space;
+
+    private BigDecimal total_space;
+
+    public Map<Object,Object> getMemory(){
+
+        String path = space.substring(0,space.indexOf(":")+1);
+        String pan=space.substring(0,space.indexOf(":"));
+        File file=new File(path);
+        Map<Object,Object> map= new ConcurrentHashMap();
+
+        //获取信息
+        BigDecimal oneGB=new BigDecimal(1024 * 1024 * 1024);
+        BigDecimal freeMemory=new BigDecimal(file.getFreeSpace());
+        BigDecimal totalMemory=new BigDecimal(file.getTotalSpace());
+
+
+        free_space=freeMemory.divide(oneGB,2, RoundingMode.HALF_UP);
+        total_space=totalMemory.divide(oneGB,2,RoundingMode.HALF_UP);
+
+
+        map.put(pan+"盘总空间",total_space+"GB");
+        map.put(pan+"盘剩余空间",free_space+"GB");
+        return  map;
+    }
+
 }
--- test/src/main/java/com/our_nacos/test/tyl/testloadbalance/Demo.java
+++ test/src/main/java/com/our_nacos/test/tyl/testloadbalance/Demo.java
@@ -2,21 +2,25 @@ package com.our_nacos.test.tyl.testloadbalance;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.our_nacos.client.environment.Environment;
 import com.our_nacos.client.loadRestTemplate.MineRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
+
 import java.net.URISyntaxException;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+
 
 @RestController
 public class Demo {
 
     @Autowired
     private MineRestTemplate mineRestTemplate;
+    
+    @Autowired
+    private Environment environment;
 
     @RequestMapping("demo")
     public String get() throws JsonProcessingException, URISyntaxException {
@@ -28,28 +32,7 @@ public class Demo {
 
     @RequestMapping("/test")
     public Map<Object,Object>   test(){
-        String s ="E:\\BaiduNetdiskDownload";
-        String path = s.substring(0,s.indexOf(":")+1);
-        String pan=s.substring(0,s.indexOf(":"));
-        File file=new File(path);
-        Map<Object,Object> map= new ConcurrentHashMap();
-//        Runtime runtime = Runtime.getRuntime();
-//        Long totalMemory = runtime.totalMemory();
-//        Long freeMemory=runtime.freeMemory();
-//        int Processors = runtime.availableProcessors();
-        double oneGB = 1024 * 1024 * 1024;
-        long freeMemory = file.getFreeSpace();
-        long totalMemory = file.getTotalSpace();
-        long usableMemory = file.getUsableSpace();
-
-        double freeMemoryGB=freeMemory/oneGB;
-        double totalMemoryGB=totalMemory/oneGB;
-        double useableMemoryGB=usableMemory/oneGB;
-
-        map.put(pan+"盘总空间",totalMemoryGB+"GB");
-        map.put(pan+"盘剩余空间",freeMemoryGB+"GB");
-        map.put(pan+"盘已用空间",useableMemoryGB+"GB");
-//        map.put("几个核",Processors);
+        Map<Object, Object> map = environment.getMemory();
         return map;
     }
 }
